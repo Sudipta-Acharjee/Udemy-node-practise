7a7eb0416d0d31e4bce3f6a0dc37199110c6cb8e
unknown <sudiptoacharjee4@gmail.com>
2021-09-16 15:23:57 +0600

    Syn & Asyn done

--- index.js
+++ index.js
@@ -11,13 +11,18 @@ console.log('File written');
 
 //Non-BLocking Asynchronous way;
 
-fs.readFile(`./1-node-farm/starter/txt/start.txt`,'utf-8',(err, data1) => {
+fs.readFile(`./1-node-farm/starter/txt/starttttttt.txt`,'utf-8',(err, data1) => {
+    if(err) return console.log("ERROR!**********")
     fs.readFile(`./1-node-farm/starter/txt/${data1}.txt`,'utf-8',(err, data2) => {
         console.log("Find the data:",data2);
         fs.readFile(`./1-node-farm/starter/txt/append.txt`,'utf-8',(err, data3)=>{
             console.log(data3);
+            fs.writeFile('./1-node-farm/starter/txt/input.txt',`${data2}\n${data3}`,'utf-8',err=>{
+                console.log("Your file has been written successfully")
+            })
         }
         )
+        
 });
 });
 console.log('Read Successfully');
