79f706402ef5c003b2054a1195873939e5b9b465
unknown <sudiptoacharjee4@gmail.com>
2021-09-17 17:32:21 +0600

    update

--- index.js
+++ index.js
@@ -3,46 +3,56 @@ const http = require('http');
 const url = require('url');
 
 // BLocking synchronous way;
-const textIn1= fs.readFileSync('./1-node-farm/starter/txt/input.txt', 'utf-8');
-console.log(textIn1);
+// const textIn1= fs.readFileSync('./1-node-farm/starter/txt/input.txt', 'utf-8');
+// console.log(textIn1);
 
-const textOut1 =  `This is what we know about the avocado: ${textIn1}.\nCreated on ${Date.now()}`;
-fs.writeFileSync('./1-node-farm/starter/txt/start.txt',textOut1);
-console.log('File written');
+// const textOut1 =  `This is what we know about the avocado: ${textIn1}.\nCreated on ${Date.now()}`;
+// fs.writeFileSync('./1-node-farm/starter/txt/start.txt',textOut1);
+// console.log('File written');
 
 
 // Non-BLocking Asynchronous way;
 
-fs.readFile(`./1-node-farm/starter/txt/starttt.txt`,'utf-8',(err, data1) => {
-    if(err) return console.log("ERROR!**********")
-    fs.readFile(`./1-node-farm/starter/txt/${data1}.txt`,'utf-8',(err, data2) => {
-        console.log("Find the data:",data2);
-        fs.readFile(`./1-node-farm/starter/txt/append.txt`,'utf-8',(err, data3)=>{
-            console.log(data3);
-            fs.writeFile('./1-node-farm/starter/txt/input.txt',`${data2}\n${data3}`,'utf-8',err=>{
-                console.log("Your file has been written successfully")
-            })
-        }
-        )
+// fs.readFile(`./1-node-farm/starter/txt/starttt.txt`,'utf-8',(err, data1) => {
+//     if(err) return console.log("ERROR!**********")
+//     fs.readFile(`./1-node-farm/starter/txt/${data1}.txt`,'utf-8',(err, data2) => {
+//         console.log("Find the data:",data2);
+//         fs.readFile(`./1-node-farm/starter/txt/append.txt`,'utf-8',(err, data3)=>{
+//             console.log(data3);
+//             fs.writeFile('./1-node-farm/starter/txt/input.txt',`${data2}\n${data3}`,'utf-8',err=>{
+//                 console.log("Your file has been written successfully")
+//             })
+//         }
+//         )
 
-});
-});
-console.log('Read Successfully');
+// });
+// });
+// console.log('Read Successfully');
 
 //Creating a SERVER
+const tempOverview = fs.readFileSync(`./1-node-farm/starter/templates/template-overview.html`, 'utf-8')
+const tempCard = fs.readFileSync(`./1-node-farm/starter/templates/template-card.html`, 'utf-8')
+const teamProduct = fs.readFileSync(`./1-node-farm/starter/templates/template-product.html`, 'utf-8')
 const data = fs.readFileSync(`./1-node-farm/starter/dev-data/data.json`, 'utf-8')
 const dataObj= JSON.parse(data);
 
 const server = http.createServer((req, res) => {
     const pathName = req.url;
+
+    //Overview page
     if (pathName === '/' || pathName === '/overview') {
         res.end('THis is the overview')
+
+    //Product page    
     } else if (pathName === '/product') {
         res.end('This is the product')
+
+    //API
     } else if (pathName === '/api') {
         res.writeHead(200,{'Content-type':'application/json'});
         res.end(data);
-        
+    
+    //NOT FOUND
     } else {
         res.writeHead(404, {
             'Content-type': 'text/html',
